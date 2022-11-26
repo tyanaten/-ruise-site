858e507e8e9e1c4b483e3f74224a41bfcde9ce78
Yana <ten777yana@mail.ru>
2022-11-26 21:46:49 +0300

    create travel section and footer

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,5 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
-import './js/main-menu.js';
 
 // ---------------------------------
 
@@ -19,6 +18,28 @@ window.addEventListener('DOMContentLoaded', () => {
   window.addEventListener('load', () => {
     initModals();
   });
+});
+
+const OPEN_STATE = 'open';
+const CLOSE_STATE = 'closed';
+
+const navToggle = document.querySelector('.main-header__toggler');
+const changedHeaderElements = document.querySelectorAll('[data-header-state="no-js"]');
+
+const setHeaderState = (state) => {
+  changedHeaderElements.forEach((element) => {
+    element.setAttribute('data-header-state', state);
+  });
+};
+
+setHeaderState(CLOSE_STATE);
+
+navToggle.addEventListener('click', () => {
+  if (navToggle.getAttribute('data-header-state') === 'closed') {
+    setHeaderState(OPEN_STATE);
+  } else {
+    setHeaderState(CLOSE_STATE);
+  }
 });
 
 // ---------------------------------
